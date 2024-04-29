d4c8fe3a5f3560c5408e0afb8aeb8eeb20a490b8
Wellington Pereira Pinto <Wellingtonp.p@hotmail.com>
2024-04-29 17:13:37 -0300

    botão de salvar cadastrando produto.

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -12,22 +12,36 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
 public class ProdutosDAO {
-    
     Connection conn;
-    PreparedStatement prep;
+    PreparedStatement prep = null;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+        try {
+            conn = new conectaDAO().connectDB();
+
+            String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+            prep = conn.prepareStatement(sql);
+
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+
+            prep.executeUpdate();
+
+            prep.close();
+            conn.close();
+
+            System.out.println("Produto cadastrado com sucesso.");
+        } catch (SQLException ex) {
+            System.out.println("Erro ao cadastrar" + ex.getMessage());
+        }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
